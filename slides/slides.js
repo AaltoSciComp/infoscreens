53670d41af59905403671e3bc745a707f49b0188
Richard Darst <rkd@zgib.net>
2023-02-13 15:41:38 +0200

    Split javascript into own file

---
+++ slides/slides.js
@@ -0,0 +1,21 @@
+/* From https://stackoverflow.com/a/11935263 */
+function getRandomSubarray(arr, size) {
+    var shuffled = arr.slice(0), i = arr.length, temp, index;
+    while (i--) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(0, size);
+}
+
+/* https://stackoverflow.com/a/901144 */
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
