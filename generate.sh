16a0044a3bfd3ca1a6eb1b403ec0a1c265845967
Richard Darst <rkd@zgib.net>
2023-02-16 17:30:10 +0200

    generate: Don't auto-generate a .html for every image file

--- generate.sh
+++ generate.sh
@@ -6,11 +6,11 @@ rsync -a slides/ $OUT/
 
 # Generat a HTML for all images (even if the image is already used in
 # some other slide)
-for x in $(find slides/ -name '*.png' -o -name '*.jpg' -o -name '*.jpeg' -o -name '*.svg') ; do
-    echo image $x
-    FILE=$(realpath --relative-to=slides/ "$x")
-    cat template-image.html | sed s/IMAGE.png/$FILE/ > www/auto-$FILE.html
-done
+#for x in $(find slides/ -name '*.png' -o -name '*.jpg' -o -name '*.jpeg' -o -name '*.svg') ; do
+#    echo image $x
+#    FILE=$(realpath --relative-to=slides/ "$x")
+#    cat template-image.html | sed s/IMAGE.png/$FILE/ > www/auto-$FILE.html
+#done
 
 # Add all HTML files to the index
 rm $OUT/index.html
